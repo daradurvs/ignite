b1fa8cc4000767f44b83458ca39bcb982aa4f263
NSAmelchev <nsamelchev@gmail.com>
2017-05-04 15:23:06 +0300

    Some fix

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryFieldAccessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryFieldAccessor.java
@@ -111,6 +111,7 @@ public abstract class BinaryFieldAccessor {
             case OBJECT_ARR:
             case BINARY_OBJ:
             case BINARY:
+            case EXTERNALIZABLE:
                 return new DefaultFinalClassAccessor(field, id, mode, false);
 
             default:
@@ -875,6 +876,7 @@ public abstract class BinaryFieldAccessor {
                     break;
 
                 case BINARY:
+                case EXTERNALIZABLE:
                 case OBJECT:
                     val = reader.readObject(id);
 
