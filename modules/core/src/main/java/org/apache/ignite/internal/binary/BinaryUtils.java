339b220307c69c29998d7b24f3aff14a67aea982
NSAmelchev <nsamelchev@gmail.com>
2017-05-11 19:46:17 +0300

    Some fix BinaryObject

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryUtils.java
@@ -1805,6 +1805,10 @@ public class BinaryUtils {
 
                 handles.setHandle(po, start);
 
+                /*Must be another way to resolve it*/
+                if (BinaryUtils.mode(ctx.descriptorForTypeId(true, po.typeId(), ldr, false).describedClass()) == BinaryWriteMode.EXTERNALIZABLE)
+                    return po.deserialize();
+
                 return po;
             }
 
