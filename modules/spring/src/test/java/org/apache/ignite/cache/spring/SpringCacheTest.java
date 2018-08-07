fe84670e0c3de09dce473a3a54bcb05a05ff7184
Vyacheslav Daradur <daradurvs@gmail.com>
2018-08-07 12:34:06 +0300

    Revert changes of SpringCacheTest

--- modules/spring/src/test/java/org/apache/ignite/cache/spring/SpringCacheTest.java
+++ modules/spring/src/test/java/org/apache/ignite/cache/spring/SpringCacheTest.java
@@ -19,22 +19,157 @@ package org.apache.ignite.cache.spring;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 /**
+ * Tests for {@link SpringCache}
  */
 public class SpringCacheTest extends GridCommonAbstractTest {
-   public void test1() throws Exception {
-       try {
-           IgniteEx ignite = startGrid(0);
+    /** */
+    private static Ignite ignite;
 
-           ignite.createCache("testCache");
+    /** Wrapped cache. */
+    private IgniteCache nativeCache;
 
-           ignite.destroyCache("testCache");
-       }finally {
-           stopAllGrids();
-       }
-   }
+    /** Working cache. */
+    private SpringCache springCache;
+
+    /** */
+    private String cacheName;
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTestsStarted() throws Exception {
+        super.beforeTestsStarted();
+
+        ignite = startGrid();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTestsStopped() throws Exception {
+        G.stop(true);
+    }
+
+    /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
+    @Override protected void beforeTest() throws Exception {
+        super.beforeTest();
+
+        cacheName = String.valueOf(System.currentTimeMillis());
+        nativeCache = ignite.getOrCreateCache(cacheName);
+        springCache = new SpringCache(nativeCache, null);
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        super.afterTest();
+
+        ignite.destroyCache(cacheName);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetName() throws Exception {
+        assertEquals(cacheName, springCache.getName());
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetNativeCache() throws Exception {
+        assertEquals(nativeCache, springCache.getNativeCache());
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetByKey() throws Exception {
+        String key = "key";
+        String value = "value";
+
+        springCache.put(key, value);
+        assertEquals(value, springCache.get(key).get());
+
+        assertNull(springCache.get("wrongKey"));
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetByKeyType() throws Exception {
+        String key = "key";
+        String value = "value";
+
+        springCache.put(key, value);
+        assertEquals(value, springCache.get(key, String.class));
+
+        try {
+            springCache.get(key, Integer.class);
+            fail("Missing exception");
+        }
+        catch (Exception e) {
+            assertTrue(e.getMessage().startsWith("Cached value is not of required type [cacheName=" + cacheName));
+        }
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testPut() throws Exception {
+        String key = "key";
+        assertNull(springCache.get(key));
+
+        String value = "value";
+        springCache.put(key, value);
+
+        assertEquals(value, springCache.get(key).get());
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testPutIfAbsent() throws Exception {
+        String key = "key";
+        String expected = "value";
+
+        assertNull(springCache.putIfAbsent(key, expected));
+
+        assertEquals(expected, springCache.putIfAbsent(key, "wrongValue").get());
+
+        assertEquals(expected, springCache.get(key).get());
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testEvict() throws Exception {
+        String key = "key";
+        assertNull(springCache.get(key));
+
+        springCache.put(key, "value");
+        assertNotNull(springCache.get(key));
+
+        springCache.evict(key);
+        assertNull(springCache.get(key));
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testClear() throws Exception {
+        String key;
+        springCache.put((key = "key1"), "value1");
+        assertNotNull(springCache.get(key));
+        springCache.put((key = "key2"), "value2");
+        assertNotNull(springCache.get(key));
+        springCache.put((key = "key3"), "value3");
+        assertNotNull(springCache.get(key));
+
+        springCache.clear();
+
+        assertNull(springCache.get("key1"));
+        assertNull(springCache.get("key2"));
+        assertNull(springCache.get("key3"));
+    }
 }
